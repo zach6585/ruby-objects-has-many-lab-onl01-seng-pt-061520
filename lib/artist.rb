0ffa81782c109e3ed11169d65725f7ffcb0ee35b
zachary mines <zmines@wesleyan.edu>
2020-07-14 20:17:33 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,27 +1,25 @@
-class Artist 
+class Artist
   attr_accessor :name
   def initialize(name)
-    @name = name 
-  end 
-  
- 
+    @name = name
+  end
+
+
   def add_song(song)
     song.artist = self
-    
-  end 
-  
+
+  end
+
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
   end
-  
+
   def self.song_count
-    Song.all.select{|song| song.artist == self}.length
-  end 
-  def songs=(songs)
-    @songs = songs 
-  end 
-  def songs 
+    Song.all.length
+
+  end
+  def songs
     @songs = Song.all.select {|song| song.artist == self}
-  end 
+  end
 end
