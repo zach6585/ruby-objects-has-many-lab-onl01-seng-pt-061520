a4b430371e7deddb8a7e079c0e56cbae460923f8
zachary mines <zmines@wesleyan.edu>
2020-07-14 20:17:44 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,32 +1,26 @@
-class Author 
-  
+
+class Author
   attr_accessor :name
-  @@count = 0
-  
   def initialize(name)
-    @name = name 
-    @posts = []
-  end 
-  
- 
-  def add_post(posta)
-    posta.author = self
-    @posts << posta
-  end 
-  
-  def add_post_by_title(pst)
-    posta = Song.new(pst)
-    @posts << posta
-    posta.author = self 
+    @name = name
   end
-  
+
+
+  def add_post(pst)
+    pst.author = self
+
+  end
+
+  def add_post_by_title(name)
+    pst = Post.new(name)
+    add_post(pst)
+  end
+
   def self.post_count
-    @@count 
-  end 
-  
-  def posts
-    Post.all.select {|pst| pst.author == self}
-    @@count += 1 
+    Post.all.length
+
   end
-  
+  def posts
+    @posts = Post.all.select {|pst| pst.author == self}
+  end
 end
